8da128901d9106664b8c6cd9444f375713ba2ec0
marcos0811 <japam620@gmail.com>
2024-07-09 16:00:08 -0500

    Update sobrecarga.sh

--- sobrecarga.sh
+++ sobrecarga.sh
@@ -9,6 +9,5 @@ stress --vm 1 --vm-bytes 1G --timeout 60
 # Comando usado para generar una carga en el disco
 dd if=/dev/zero of=/tmp/testfile bs=1M count=1024 oflag=direct 
 
-# Esperamos a que todas las tareas en segundo plano terminen
-wait
+
 echo "Prueba de carga completada"
