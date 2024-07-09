608c900cf2694ca9d7140305e8f91cca54b680a2
marcos0811 <japam620@gmail.com>
2024-07-09 16:36:59 -0500

    Update grafica.py

--- grafica.py
+++ grafica.py
@@ -5,22 +5,22 @@ import pwd
 
 # Obtener el nombre de usuario actual
 usuario = pwd.getpwuid(os.geteuid()).pw_name
-df = pd.read_csv(f"/home/{usuario}/Escritorio/stats.csv") # ingresamos la ruta de eodne esta el archivo csv
+df = pd.read_csv(f"/home/{usuario}/Escritorio/stats.csv") # ingresamos la ruta de donde esta el archivo csv
 
 
 # Convertimos la columna 'Tiempo' a formato de fecha y hora
 df['Timestamp'] = pd.to_datetime(df['Timestamp'])
 
 
-# Graficar del consumo de memoria
+# Graficamos el consumo de memoria
 plt.plot(df['Timestamp'], df['RAM Usage (%)'],
          color='blue', label='Memoria(%)', marker='o')
 
-# Graficar cel uso de CPU
+# Graficamos el uso de CPU
 plt.plot(df['Timestamp'], df['CPU Usage (%)'],
          color='red', label='CPU(%)', marker='o')
 
-# Configuracion de la grafica
+# Configuramos las credenciales de la grafica
 plt.xlabel('Tiempo')
 plt.ylabel('Uso (%)')
 plt.title('Consumo de la Memoria RAM y CPU a lo largo del tiempo')
