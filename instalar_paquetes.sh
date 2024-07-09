e72462bb6db2ea9b148a0aec676b9c38222fb7e5
marcos0811 <japam620@gmail.com>
2024-07-08 22:21:34 -0500

    Update instalar_paquetes.sh

--- instalar_paquetes.sh
+++ instalar_paquetes.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
-# Actualizar lista de paquetes
+# Actualizamos lista de paquetes
 echo "Actualizando la lista de paquetes..."
 sudo apt-get update
 
-# Instalar Python 3
+#Instalamos msmtp para el envio de correos
+echo "Instalando msmtp..."
+sudo apt-get install -y msmtp
+
+# Instalamos Python 3 para la creacion de la grafica
 echo "Instalando Python 3..."
 sudo apt-get install -y python3
 
-# Instalar pip para Python 3
+# Instalamos pip para Python 3
 echo "Instalando pip para Python 3..."
 sudo apt-get install -y python3-pip
 
-# Instalar pandas y matplotlib usando pip
+# Instalamos pandas y matplotlib usando pip
 echo "Instalando pandas y matplotlib..."
 pip3 install pandas matplotlib
 
-# Instalar stress
+# Instalamos stress para generar una sobrecarga en els sistema con comandos especificos
 echo "Instalando stress..."
 sudo apt-get install -y stress
 
