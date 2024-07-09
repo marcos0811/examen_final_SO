53333186916d23ff04bfb36baf1523be7d6bb8f7
marcos0811 <japam620@gmail.com>
2024-07-09 15:55:58 -0500

    Update sobrecarga.sh

--- sobrecarga.sh
+++ sobrecarga.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Generamos una carga en el CPU haciendo uso del paquete stress
-stress --cpu $(nproc) --timeout 60 &
+stress --cpu $(nproc) --timeout 60 
 
 # Generar carga de memoria haciendo uso del paquete stress
-stress --vm 1 --vm-bytes 1G --timeout 60 &
+stress --vm 1 --vm-bytes 1G --timeout 60 
 
 # Comando usado para generar una carga en el disco
-dd if=/dev/zero of=/tmp/testfile bs=1M count=1024 oflag=direct &
+dd if=/dev/zero of=/tmp/testfile bs=1M count=1024 oflag=direct 
 
 # Esperamos a que todas las tareas en segundo plano terminen
 wait
